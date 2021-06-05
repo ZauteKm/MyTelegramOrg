9cdec1dbfae9e6b184d3c6c48cf0479139be40ad
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-05 15:22:00 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,8 @@
+class Translation(object):
+    START_TEXT = """<b>Hi!</b>\nThis Is An <b>APP ID & HASH GENERATOR BOT.</b>\n\nI Can Help You Get Your <b>Telegram APP ID & HASH,</b> Just Make Sure You Read Terms & Conditions Of Running This Bot In The <b>Disclaimer</b> Page Firstly.\n\n<b>Enter Your Telegram Number (Including Country Code For Example +16374826473) To Get The APP-ID From my.telegram.org</b>\n\nPress /start At Any Stage To <b>Re-Enter Your Details If You Perhaps Made Some Mistake.</b>"""
+    AFTER_RECVD_CODE_TEXT = """A Verification Code sent to you by Telegram. Enter that Verification Code Or Forward That Verification Message."""
+    BEFORE_SUCC_LOGIN = "Recieved Code. Scarpping Web Page ..."
+    ERRED_PAGE = "Something Went Wrong. Failed To Get App ID. \n\n@ZauteBot"
+    CANCELLED_MESG = "Bye! Please Reply /start the bot conversation"
+    IN_VALID_CODE_PVDED = "Sorry, But The Input Does Not Seem To Be A Valid Telegram Web-Login Code"
+    IN_VALID_PHNO_PVDED = "Sorry, But The Input Does Not Seem To Be A Valid Phone Number"
