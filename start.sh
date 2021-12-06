06fa02f44e74d2b5099703a54edddfaf5292d257
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-06 23:16:53 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/MyTelegramOrg.git /MyTelegramOrg
-cd /MyTelegramOrg
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
