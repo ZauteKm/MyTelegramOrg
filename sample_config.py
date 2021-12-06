5e07554e4a1df20255f0eb580df4e4500ce05ad3
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-12-06 23:23:52 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -29,7 +29,7 @@ class Config(object):
     # if any of the platform, does not work
     # please reopen
     # https://github.com/LushaiTech/MyTelegramORG/issues/3
-    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/UseTGzKbot")
+    APP_DESCRIPTION = os.environ.get("APP_DESCRIPTION", "created using https://telegram.dog/UseTGOrgBot")
     #
     FOOTER_TEXT = os.environ.get("FTEXT", "Please Subscribe ❤️ @ZauteKm")
     # the strings used in the different messages
